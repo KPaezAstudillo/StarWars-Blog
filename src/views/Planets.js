944cf5e393cd8573ff760f400925f55ae38c01b3
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-11-29 14:52:09 -0300

    fetch planets

--- src/views/Planets.js
+++ src/views/Planets.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom";
+import { getPlanets } from '../api/fetch'
+import "../styles/cards.css"
 
 export default function Planets() {
-  return (
-    <div>Planets</div>
-  )
+    const [planets, setPlanets] = useState([])
+    useEffect(() => {
+        getPlanets().then(setPlanets);
+console.log(planets?.results?.url);
+    }, [planets]);
+    return (
+        <><h1>Planets</h1>
+            <div className='row d-flex'>
+                {
+                    planets?.results?.map((planet) =>
+                    (
+                        <div className="card col-md-3 col-6 " key={planet.name}>
+                            <img src={`https://starwars-visualguide.com/assets/img/planets/${Number(planet.url.split('/').slice(-2)[0])}.jpg`} className="card-img-top" alt="..." />
+                            <div className="card-body" >
+                                <h5 className="card-title">{planet.name}</h5>
+                                <p className="card-text">
+                                  ...
+                                </p>
+                                <Link to={`./${Number(planet.url.split('/').slice(-2)[0])}/details`} className="card-link me-2" >Detail</Link>
+
+                                <Link to="/" className="btn btn-primary w-50">Add </Link>
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    )
 }
--- src/views/singleCharacter.js
+++ src/views/singleCharacter.js
@@ -10,24 +10,36 @@ export default function SingleCharacter() {
 
     }, [id]);
     return (
-        <div className='row'>
-            <div className=" mx-auto card col-md-8 col " key={singleChar.name}>
-                <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} className="card-img-top mt-3 w-75 h-75 mx-auto" alt="..." />
-                <div className="card-body  mx-auto" >
-                    <h4 className="card-title">{singleChar.name}</h4>
-                    <p className="card-text">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item px-0">Gender: {singleChar.gender}</li>
-                            <li class="list-group-item px-0">Mass: {singleChar.mass}</li>
-                            <li class="list-group-item px-0">Height: {singleChar.height}</li>
-                            <li class="list-group-item px-0">Hair Color: {singleChar.hair_color}</li>
-                            <li class="list-group-item px-0">Skin Color: {singleChar.skin_color}</li>
-                            <li class="list-group-item px-0">Eye Color: {singleChar.eye_color}</li>
-                            <li class="list-group-item px-0">Birth Year: {singleChar.birth_year}</li>
-                        </ul>
-                    </p>
+        <>
+            <div className="d-flex justify-content-center font-weight-bold">
+                 <h1 className='font-weight-bold' >{singleChar.name}</h1>               
+            </div>
+            <div className='m-3 row d-flex'>
+                <div className=" mx-auto card col-md-8 col-12 " key={singleChar.name}>
+                    <div className='row'>
+                        <div className='col-md-9 col-12'>
+                            <img src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`} className="card-img-top h-100  " alt="..." />
+                        </div>
+                        <div className="card-body col-md-3 col-12 my-auto" >
+
+                            <p className="card-text">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item px-0">Gender: {singleChar.gender}</li>
+                                    <li class="list-group-item px-0">Mass: {singleChar.mass}</li>
+                                    <li class="list-group-item px-0">Height: {singleChar.height}</li>
+                                    <li class="list-group-item px-0">Hair Color: {singleChar.hair_color}</li>
+                                    <li class="list-group-item px-0">Skin Color: {singleChar.skin_color}</li>
+                                    <li class="list-group-item px-0">Eye Color: {singleChar.eye_color}</li>
+                                    <li class="list-group-item px-0">Birth Year: {singleChar.birth_year}</li>
+                                </ul>
+                                <p className='mt-5'> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed interdum urna quis leo auctor, quis placerat nisi finibus. In eu iaculis est, ac laoreet libero. Cras sagittis quam vel feugiat gravida. Maecenas commodo dolor quis nisi placerat, vel commodo ligula ornare. Aliquam erat volutpat. In eu vulputate diam. Nam consequat lacus ut augue semper, nec interdum nisl egestas. Nullam quis pretium enim.
+
+                                    Proin ornare eros velit, non dignissim tellus cursus a. Aenean viverra metus elit, eget sagittis velit lacinia sit amet. Integer ullamcorper et justo at varius. Suspendisse mattis accumsan mauris, sed sagittis nulla laoreet eget. Proin non cursus dolor. Aenean congue tristique est, eget vehicula felis vestibulum at. Vestibulum mauris massa, mattis eget rutrum eu, aliquet quis turpis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vulputate quis risus nec malesuada. Mauris eget velit ullamcorper, molestie enim at, rhoncus nulla. Nunc mollis nunc congue molestie imperdiet. Morbi eget augue blandit, facilisis urna at, vestibulum velit. Maecenas porttitor dui lectus. Nam metus nisi, volutpat eget dapibus quis, sollicitudin eu diam. Donec ac est a urna euismod rhoncus et sit amet velit.  </p>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
