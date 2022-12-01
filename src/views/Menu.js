0bdfe923696c658404b9ed6545d6831cec83775c
KPaezAstudillo <karinapaezastudillo@gmail.com>
2022-11-30 23:06:29 -0300

    finished

--- src/views/Menu.js
+++ src/views/Menu.js
@@ -11,21 +11,20 @@ export default function Menu() {
         <h1 className='d-flex justify-content-center'>Choose one:</h1>
             <div className='row my-5'>
                 <div className='col-12 col-md-3 mx-auto mb-3'>
-                    <Link to="/characters">
+                    <Link to="/characters" className='button-menu'>
                         <img src="https://media.timeout.com/images/105863223/750/422/image.jpg" className='menu-img w-100' />
                         <h2 className='d-flex justify-content-center '>Characters</h2>
                     </Link>
                     
                 </div>
-                <div className='col-12 col-md-3 mx-auto mb-3'>
-                    <Link to="/planets">
+                <div className='col-12 col-md-3 mx-auto mb-3'>                   
+                    <Link to="/planets" className='button-menu'>
                         <img src="https://www.famousbollywood.com/wp-content/uploads/2021/05/Best-Planets-to-Live-on-in-Star-Wars.jpg" className='menu-img w-100' />
                         <h2 className='d-flex justify-content-center'>Planets</h2>
-                    </Link>
-                    
+                    </Link>                    
                 </div>
                 <div className='col-12 col-md-3 mx-auto mb-3'>
-                    <Link to="/vehicles ">
+                    <Link to="/vehicles " className='button-menu'>
                         <img src="https://dwgyu36up6iuz.cloudfront.net/heru80fdn/image/upload/c_fill,d_placeholder_wired.png,fl_progressive,g_face,h_450,q_80,w_800/v1576594418/wired_each-and-every-starfighter-in-star-wars-explained.jpg" className='menu-img w-100' />
                         <h2 className='d-flex justify-content-center'>Vehicles</h2>
                     </Link>
